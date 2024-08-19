efc2ce349d3e83a58c8aa910a294f3f5fdcfaf13
Luca Gaziglia <luca.gaziglia@promored.net>
2024-08-19 12:55:04 -0300

    deje todo como estaba en el inicio

--- 00_usando_git/luquita.java
+++
@@ -1,5 +0,0 @@
-public class luquita {
-    public static void main(String[] args) {
-        System.out.println("Hola puto");
-    }
-}
--- 00_usando_git/src/Lamparita.java
+++
@@ -1,5 +0,0 @@
-public class Lamparita {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Vamos a programar una lamparita! ");
-    }
-}
